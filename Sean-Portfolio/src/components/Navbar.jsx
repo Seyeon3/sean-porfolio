e2e64cb5e50a4c4f9d46f40651bb71e2f057de46
sean <seanmanaog22@gmail.com>
2025-08-30 12:19:04 +0800

    goods

--- Sean-Portfolio/src/components/Navbar.jsx
+++ Sean-Portfolio/src/components/Navbar.jsx
@@ -67,6 +67,7 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
               { name: "About", href: "#about" },
               { name: "Skills", href: "#skills" },
               { name: "Project", href: "#project" },
+              { name: "Cerfications", href: "#certificates" },
               { name: "Contact", href: "#contact" },
             ].map((link) => (
               <a
